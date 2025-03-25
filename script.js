ae3809fabfe8625095b63fee809e71aec02359ff
nickdowski <nickdowski@gmail.com>
2025-03-25 13:44:38 -0500

    Create basic calculator functions

---
+++ script.js
@@ -0,0 +1,8 @@
+//Addition
+const add = (num1, num2) => num1 + num2;
+//Subtraction
+const subtract = (num1, num2) => num1 - num2;
+//Multiplication
+const multiply = (num1, num2) => num1 * num2;
+//Division
+const divide = (num1, num2) => num1 / num2;
