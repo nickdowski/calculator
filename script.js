696708f5c583b57437e98f737ae46e97121c34a9
nickdowski <nickdowski@gmail.com>
2025-03-25 13:52:55 -0500

    Create the 'operate' function to switch operators based on the case

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 //Division
 const divide = (num1, num2) => num1 / num2;
+    if (num === 0) {
+        return "Nice try, buddy boy!"
+    }
+    else {
+        return num1 / num2;
+    }
+
+//Operate
+const operate = (operator, num1, num2) => {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
