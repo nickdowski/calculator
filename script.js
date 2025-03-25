0f1f8ed6dfd52a26bb3f218952c39e925b542e56
nickdowski <nickdowski@gmail.com>
2025-03-25 15:13:26 -0500

    Update styling

--- script.js
+++ script.js
@@ -26,3 +26,62 @@ const operate = (operator, num1, num2) => {
             return divide(num1, num2);
     }
 }
+
+//Display
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+let displayValue = '';
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let result = '';
+let operatorClicked = false;
+let equalsClicked = false;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('number')) {
+            if (equalsClicked) {
+                displayValue = '';
+                firstNumber = '';
+                secondNumber = '';
+                operator = '';
+                result = '';
+                operatorClicked = false;
+                equalsClicked = false;
+            }
+            displayValue += button.textContent;
+            display.textContent = displayValue;
+        }
+        if (button.classList.contains('operator')) {
+            if (operatorClicked) {
+                firstNumber = operate(operator, parseFloat(firstNumber), parseFloat(displayValue));
+                display.textContent = firstNumber;
+                displayValue = '';
+                operator = button.textContent;
+            }
+            if (!operatorClicked) {
+                firstNumber = displayValue;
+                operator = button.textContent;
+                operatorClicked = true;
+                displayValue = '';
+            }
+        }
+        if (button.classList.contains('equals')) {
+            secondNumber = displayValue;
+            result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+            display.textContent = result;
+            equalsClicked = true;
+        }
+        if (button.classList.contains('clear')) {
+            displayValue = '';
+            firstNumber = '';
+            secondNumber = '';
+            operator = '';
+            result = '';
+            operatorClicked = false;
+            equalsClicked = false;
+            display.textContent = displayValue;
+        }
+    })
+}
